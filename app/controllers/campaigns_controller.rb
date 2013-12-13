de1b72c82d7523caf450972ff82f06026037dfeb
Nichol Alexander <nichol.alexander@gmail.com>
2013-12-13 15:51:47 -0500

    build form

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -1,6 +1,7 @@
 class CampaignsController < ApplicationController
 
   def new
+    @campaign = Campaign.new
   end
 
 
--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -2,4 +2,10 @@ describe Organization do
 
   it { should have_many(:campaigns) }
 
+  it "can be created" do
+    org = Organization.new
+
+    expect(org).to not_be(nil)
+  end
+
 end
