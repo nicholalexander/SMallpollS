ce168da254f37ee609f44f836ed55c290d0d4f30
Nichol Alexander <nichol.alexander@gmail.com>
2013-12-16 12:39:45 -0500

    migration for fk for campaigns, green rspec

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131213184142) do
+ActiveRecord::Schema.define(version: 20131216173354) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "campaigns", force: true do |t|
-    t.string "name"
-    t.string "description"
+    t.string  "name"
+    t.string  "description"
+    t.integer "organization_id"
   end
 
   create_table "organizations", force: true do |t|
